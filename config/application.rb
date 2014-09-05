ecd15429f38f8ba7eed1dff286c2c5d7d02dbf2a
Ricardo <bigardone@gmail.com>
2014-09-05 17:00:08 +0200

    Use sass syntax instead of scss

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module RailsAndReact
 
     # For not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # sass over scss
+    config.sass.preferred_syntax = :sass
   end
 end
