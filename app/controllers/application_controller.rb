1fa772642e947dfdb2c5ecfb3963500c021e7cb3
Ricardo <bigardone@gmail.com>
2014-11-26 08:38:56 +0100

    Use Gon to set Analytics id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_gon_variables
+
+  protected
+
+  def set_gon_variables
+    gon.google_analytics_id = Rails.application.secrets[:google_analytics_id]
+  end
 end
